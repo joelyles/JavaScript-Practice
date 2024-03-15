327549edd9ed112f37b631051626ce1717a531e1
Joe Lyles <lylesjoe@protonmail.com>
2024-03-15 15:15:09 -0400

    set up practice server

---
+++ server.js
@@ -0,0 +1,23 @@
+// practice server using mostly Dave Gray node tutorial notes/code and original documentation (express, mdm, etc.)
+
+const express = require('express');
+const server = express();
+const router = express.Router();
+const port = 3500;
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+server.use(express.urlencoded({ extended: false }));
+server.use(express.json());
+
+server.use(express.static(path.join(__dirname, '/public')));
+
+server.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+server.get('/about.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+});
+
+server.listen(port, () => console.log(`practice server running on port ${port}`));
