006f6815a4233cd5640b7d2a0445a1a7916da524
Joe Lyles <lylesjoe@protonmail.com>
2024-03-16 04:59:02 -0400

    event logger

--- server.js
+++ server.js
@@ -6,17 +6,20 @@ const router = express.Router();
 const port = 3500;
 const fsPromises = require('fs').promises;
 const path = require('path');
+const { logger } = require('./middleware/logEvents');
+
+server.use(logger);
 
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 
 server.use(express.static(path.join(__dirname, '/public')));
 
-server.get('/index.html', (req, res) => {
+server.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-server.get('/about.html', (req, res) => {
+server.get('/about(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'))
 });
 
