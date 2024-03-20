78805a3dac3373ebe6af3be6a0cfbdea96c9d81b
Joe Lyles <lylesjoe@protonmail.com>
2024-03-20 06:02:22 -0400

    set up update, delete, etc. in controller and started user registration controller

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -23,17 +23,38 @@ const createNewEmployee = (req, res) => {
     res.status(201).json(data.employees);
 }
 const updateEmployee = (req, res) => {
-    res.json({
-        "firstname": req.body.firstname,
-        "lastname": req.body.lastname,
-    });
+    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+
+    if (!employee) {
+        return res.status(400).json({ 'message': `employee ID ${req.body.id} not available` });
+    }
+
+    if (req.body.firstname) 
+        employee.firstname = req.body.firstname;
+    if (req.body.lastname) 
+        employee.lastname = req.body.lastname;
+
+    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+    const unsortedArray = [...filteredArray, employee];
+    data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
+    res.json(data.employees);
 }
 const deleteEmployee = (req, res) => {
-    res.json({ "id": req.body.id });
+    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+    if (!employee) {
+        return res.status(400).json({ 'message': `employee ID ${req.body.id} not found` });
+    }
+    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+    data.setEmployees([...filteredArray]);
+    res.json(data.employees);
 }
 
 const getEmployee = (req, res) => {
-    res.json({ "id": req.params.id });
+    const employee = data.employees.find(emp => emp.id === parseInt(req.params.id));
+    if (!employee) {
+        return res.status(400).json({ 'message': `employee ID ${req.params.id} not avalable` });
+    }
+    res.json(employee);
 }
 
 module.exports = {
---
+++ controllers/userRegistration.js
@@ -0,0 +1,15 @@
+// practice server using Dave Gray node tutorial
+
+const userDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+}
+
+const fsPromises = require('fs').promises;
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'you must have username and password' });
+}
